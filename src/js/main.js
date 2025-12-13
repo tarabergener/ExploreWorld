05ee6c82b3ba94492e79b7318a4cd6dc9e7cd723
Tara Bergener <tara.kelly211@gmail.com>
2025-12-13 13:39:36 -0500

    Added header and footer partials.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import "../style/style.css";
 import { setupCounter } from "./counter.js";
 import { getData } from "./productData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 getData("flights").then((data) => {
   console.log("API DATA:", data);
---
+++ src/js/utils.mjs
@@ -0,0 +1,42 @@
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+export async function renderHTMLWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = "true"
+) {
+  // get template using function...no need to loop this time.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if (callback) {
+    callback(data);
+  }
+}
+
+function loadTemplate(path) {
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+  };
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplateFn = loadTemplate("/src/public/partials/header.html");
+  const footerTemplateFn = loadTemplate("/src/public/partials/footer.html");
+  const header = qs("header");
+  const footer = qs("footer");
+
+  await renderHTMLWithTemplate(headerTemplateFn, header);
+  await renderHTMLWithTemplate(footerTemplateFn, footer);
+}
